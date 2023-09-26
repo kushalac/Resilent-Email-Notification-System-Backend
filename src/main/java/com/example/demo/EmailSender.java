3d6fc4821e238c94ce5ade1e32e46a3d1571e7cf
suhas <suhasmjnv17@gmail.com>
2023-09-26 17:41:00 +0530

    second commit

--- src/main/java/com/example/demo/EmailSender.java
+++ src/main/java/com/example/demo/EmailSender.java
@@ -3,37 +3,51 @@ package com.example.demo;
 import javax.mail.*;
 import javax.mail.internet.*;
 import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class EmailSender {
-    public static void sendEmail(String recipient, String subject, String content) {
-    	final String senderEmail = "notificationsystem03@gmail.com";  
-        final String senderPassword = "tgqfhjwyjwfhdmvn"; 
-        Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
+    // Create a thread pool with multiple threads to send emails concurrently.
+    private static final ExecutorService executorService = Executors.newFixedThreadPool(50); // Adjust the thread pool size as needed
 
-        Session session = Session.getInstance(props, new Authenticator() {
-            @Override
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(senderEmail, senderPassword);
+    public static void sendEmail(String recipient, String subject, String content) {
+        executorService.submit(() -> {
+            final String senderEmail = "notificationsystem03@gmail.com";
+            final String senderPassword = "tgqfhjwyjwfhdmvn";
+            Properties props = new Properties();
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.starttls.enable", "true");
+            props.put("mail.smtp.host", "smtp.gmail.com");
+            props.put("mail.smtp.port", "587");
+
+            Session session = Session.getInstance(props, new Authenticator() {
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(senderEmail, senderPassword);
+                }
+            });
+
+            try {
+                Message message = new MimeMessage(session);
+                message.setFrom(new InternetAddress(senderEmail));
+                message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipient));
+                message.setSubject(subject);
+                message.setContent(content, "text/html");
+
+               
+
+                Transport.send(message);
+            } catch (MessagingException e) {
+                System.err.println("Error sending email: " + e.getMessage());
+                e.printStackTrace();
             }
         });
+    }
 
-        try {
-            Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(senderEmail));
-            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipient));
-            message.setSubject(subject);
-            message.setContent(content, "text/html");
-
-            Transport.send(message);
-
-            System.out.println("Email sent successfully to " + recipient);
-        } catch (MessagingException e) {
-            System.err.println("Error sending email: " + e.getMessage());
-            e.printStackTrace();
-        }
+    // Shutdown the executor service when the application exits.
+    public static void shutdownExecutorService() {
+        executorService.shutdown();
     }
 }
--- src/main/java/com/example/demo/KafkaTopicConfiguration.java
+++ src/main/java/com/example/demo/KafkaTopicConfiguration.java
@@ -9,7 +9,10 @@ import org.springframework.context.annotation.Configuration;
 
 import java.util.Collections;
 import java.util.Properties;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Configuration
 public class KafkaTopicConfiguration {
@@ -17,61 +20,74 @@ public class KafkaTopicConfiguration {
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
 
+    // Create a thread pool with a single thread to create Kafka topics asynchronously.
+    private static final ExecutorService executorService = Executors.newSingleThreadExecutor();
+
     @Bean
-    public NewTopic signupTopic() {
+    public NewTopic signupTopic() throws ExecutionException, InterruptedException {
         return createNewTopic("signup-topic");
     }
 
     @Bean
-    public NewTopic promotionsTopic() {   //add validity or set time period
+    public NewTopic promotionsTopic() throws ExecutionException, InterruptedException {
         return createNewTopic("promotions-topic");
     }
 
     @Bean
-    public NewTopic latestPlansTopic() {
+    public NewTopic latestPlansTopic() throws ExecutionException, InterruptedException {
         return createNewTopic("latestPlans-topic");
     }
 
     @Bean
-    public NewTopic releaseEventsTopic() {
+    public NewTopic releaseEventsTopic() throws ExecutionException, InterruptedException {
         return createNewTopic("releaseEvents-topic");
     }
-    
+
     @Bean
-    public NewTopic accountDeletedTopic() {
+    public NewTopic accountDeletedTopic() throws ExecutionException, InterruptedException {
         return createNewTopic("account-deleted-topic");
     }
-    
+
     @Bean
-    public NewTopic accountModifiedTopic() {
+    public NewTopic accountModifiedTopic() throws ExecutionException, InterruptedException {
         return createNewTopic("account-modified-topic");
     }
-    
+
     @Bean
-    public NewTopic accountConfirmDeletedTopic() {
+    public NewTopic accountConfirmDeletedTopic() throws ExecutionException, InterruptedException {
         return createNewTopic("deleteConfirmation-topic");
     }
-    
-    private NewTopic createNewTopic(String topicName) {
-        Properties adminConfig = new Properties();
-        adminConfig.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
 
-        try (AdminClient adminClient = AdminClient.create(adminConfig)) {
-            // Check if the topic already exists
-            if (!adminClient.listTopics().names().get().contains(topicName)) {
-                // If the topic doesn't exist, create it
-                NewTopic newTopic = new NewTopic(topicName, 1, (short) 1);
-                adminClient.createTopics(Collections.singleton(newTopic)).all().get();
-                return newTopic;
-            } else {
-                // Topic already exists, print a message
-                System.out.println("Topic '" + topicName + "' already exists.");
-                return null;
+    private NewTopic createNewTopic(String topicName) throws ExecutionException, InterruptedException {
+        CompletableFuture<NewTopic> future = CompletableFuture.supplyAsync(() -> {
+            Properties adminConfig = new Properties();
+            adminConfig.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+
+            try (AdminClient adminClient = AdminClient.create(adminConfig)) {
+                // Check if the topic already exists
+                if (!adminClient.listTopics().names().get().contains(topicName)) {
+                    // If the topic doesn't exist, create it
+                    NewTopic newTopic = new NewTopic(topicName, 1, (short) 1);
+                    adminClient.createTopics(Collections.singleton(newTopic)).all().get();
+                    return newTopic;
+                } else {
+                    // Topic already exists, print a message
+                    System.out.println("Topic '" + topicName + "' already exists.");
+                    return null;
+                }
+            } catch (InterruptedException | ExecutionException e) {
+                // Handle any exceptions here
+                e.printStackTrace();
+                throw new RuntimeException("Failed to create or check topic: " + topicName, e);
             }
-        } catch (InterruptedException | ExecutionException e) {
-            // Handle any exceptions here
-            e.printStackTrace();
-            throw new RuntimeException("Failed to create or check topic: " + topicName, e);
-        }
+        }, executorService);
+
+        // Wait for the CompletableFuture to complete and return the result.
+        return future.get();
+    }
+
+    // Shutdown the executor service when the application exits.
+    public static void shutdownExecutorService() {
+        executorService.shutdown();
     }
 }
